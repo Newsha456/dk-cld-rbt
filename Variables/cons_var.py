6f1ee794f1597d5b3dd1f090c1365b84fa5fe213
Newsha <newsha@Newshas-MacBook-Air.local>
2023-06-20 07:59:28 +0330

    vars change

--- Variables/cons_var.py
+++ Variables/cons_var.py
@@ -1,3 +1,10 @@
+import    random 
+import    string 
+def generate_random_string(length):
+    letters = string.ascii_letters  # Contains both uppercase and lowercase letters
+    random_string = ''.join(random.choice(letters) for _ in range(length))
+    return random_string
+
 DIGI_CLOUD_URL  =           "https://console.digicloud.dev/"
 CHROME =                    "Chrome"
 LOGIN_PG =                  "class=auth"
@@ -14,3 +21,20 @@ REGION_SELECTOR =           "class=dropdown__toggle region-selection__toggle"
 REGION_NAME =               " ir-vanak-plaza "
 DROPDOWN_DESCRIPTION =      "class='header__services-dropdown-menu-intro text-center'"
 CLOUD_COMPUTING =           "//*[@id='app]/div[2]/header/nav/div/div[2]/div[1]/div[2]/ul/li[1]/span[1]"
+INS_LIST_PG =               "class=instances-list"
+ADD_INS =                   "Add Instance" 
+CREATE_STEP_ONE =           "Server Specifications"
+CONFIRM_BTN =               " Confirm and Continue  "
+ADVANCE_STNG =              "class=h2_4de9b95f4acab1df1f60b1b927465bf4"
+INS_NAME_INPUT =            "id:Servers Name"
+INS_NAME =                  generate_random_string(6).lower()
+IMAGE_TYPE_UBUNTU =         "Ubuntu"
+INS_TYPE =                  " g1.medium "
+CREATE_BTN =                "//*[@id='app']/div[2]/main/div[2]/div/div[2]/div/div[2]/div/div[3]/button"
+CREATING_STATE =            "Creating"
+INS_STNG =                  "//*[@id=app]/div[2]/main/div[2]/div/div[1]/div/div[2]/div/div[1]/div[1]/button"
+DELETE_INS =                "Delete"
+DELETE_INS_POPUP =          "//*[@id=app]/div[2]/main/div[2]/div/div[1]/div/div[2]/div/div[2]/div/div[2]"      
+ROOT_VOLUME_DELETE =        "class=deleteRootVolume"
+DELETE_BTN =                "//*[@id=app]/div[2]/main/div[2]/div/div[1]/div/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/button[2]"
+ACTIVE_STATUS =             "ACTIVE"
--- console.robot
+++ console.robot
@@ -34,16 +34,29 @@ Digicloud login
     Wait Until Page Contains                    Price update notice                5seconds
     Click Element                               ${GOT_IT_BTN}
     Wait Until Page Contains Element            ${HOME_PAGE}
+
 Instance create    
     Element Should Contain                      ${REGION_SELECTOR}              ${REGION_NAME} 
     Click Element                               ${SERVICE_DROPDOWN}
     Wait Until Page Contains Element            ${DROPDOWN_DESCRIPTION}
     Click Element                               ${CLOUD_COMPUTING}
-    Sleep    5second
-    
+    Wait Until Page Contains Element            ${INS_LIST_PG}                  5seconds
+    Click Element                               ${ADD_INS}
+    Page Should Contains Element                ${CREATE_STEP_ONE}
+    Click Element                               ${IMAGE_TYPE_UBUNTU}
+    Click Element                               ${INS_TYPE}
+    Click Element                               ${CONFIRM_BTN}
+    Page Should Contains Element                ${ADVANCE_STNG}
+    Sleep     5second
+    Click Element                               ${CONFIRM_BTN}
+    Input Text                                  ${INS_NAME_INPUT}                 ${INS_NAME}
+    Page Should Contains Element                ${IMAGE_TYPE_UBUNTU}       ,      ${INS_TYPE}        ,      ${INS_NAME}
+    Click Element                               ${CREATE_BTN}
+    wait until page dos not conatin element     ${CREATING_STATE}
 
 Check Instance Details
-
+    Click element                               ${INS_NAME} 
+    Page Should Contains Element                
 
 
 Delete Instance
